e7c0529513d33ed92f9deca92b3a400001115c24
Alumno <Alumno@TECT-TD04>
2023-04-27 16:47:34 -0500

    First Commit

---
+++ src/main/java/com/tecsup_lab03/lab03_4c24_GPDJ/App.java
@@ -0,0 +1,13 @@
+package com.tecsup_lab03.lab03_4c24_GPDJ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/tecsup_lab03/lab03_4c24_GPDJ/AppTest.java
@@ -0,0 +1,20 @@
+package com.tecsup_lab03.lab03_4c24_GPDJ;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
